22a5bdf2db83b94c6e837cc52b61af722f736c33
humdi85 <applemonry@gmail.com>
2025-02-20 04:44:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function processPayment() {
     alert("Payment process will start here.");
 }
+async function connectWallet() {
+    if (window.ethereum) {
+        try {
+            await window.ethereum.request({ method: "eth_requestAccounts" });
+            document.getElementById("status").innerText = "Wallet Connected!";
+        } catch (error) {
+            document.getElementById("status").innerText = "Connection Failed!";
+        }
+    } else {
+        alert("Please install MetaMask!");
+    }
+}
+
+function processPayment() {
+    alert("Payment process will start here.");
+}
